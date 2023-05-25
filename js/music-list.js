592631aa6b359f738c028d257bea8add166fc976
Ng Long Front <nglong.frontreact.2000@gmail.com>
2023-05-25 21:06:41 +0700

    firts commit

---
+++ js/music-list.js
@@ -0,0 +1,45 @@
+let allMusic = [
+  {
+    name: "Trời Giấu Trời Mang Đi",
+    artist: "Amee x ViruSs",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Anh Không Thể",
+    artist: "MoNo",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Tình Thật Tình Mơ",
+    artist: "Mer",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Buông Đôi Tay Nhau Ra",
+    artist: "Sơn Tùng MTP",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Chạy Ngay Đi",
+    artist: "Sơn Tùng MTP",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Khuôn Mặt Đáng Thương",
+    artist: "Sơn Tùng MTP",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Lạc Trôi",
+    artist: "Sơn Tùng MTP",
+    img: "music-7",
+    src: "music-7"
+  },
+
+];
